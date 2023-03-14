afae1cb396bc19977e4f6a21296c5ee5a6f92414
saaasuuukaae <philipchef13@gmail.com>
2023-03-14 17:30:35 +0200

    delete

--- server/api/ninja.ts
+++
@@ -1,8 +0,0 @@
-
-export default defineEventHandler(async (event) => {
-
-  const {data} = await $fetch<{data: any}>("https://api.currencyapi.com/v3/latest?apikey=LHuKhaWgC8DO2QaoKaWZmiLOTFWgYvkzM9LGGJlB")
-
-  return data;
-  
-})
